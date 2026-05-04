a2bb9448e5031aaa584fe1c9d78a88717198b871
dtc235200292-star <dtc235200292@ictu.edu.vn>
2026-05-04 13:04:35 +0700

    Update views.py

--- accounts/views.py
+++ accounts/views.py
@@ -1,24 +1,19 @@
-from django.contrib import messages
+from django.shortcuts import render, redirect
 from django.contrib.auth import login
 from django.contrib.auth.forms import UserCreationForm
-from django.db import IntegrityError
-from django.shortcuts import render, redirect
+from django.contrib import messages
+
 
 def register(request):
-    form = UserCreationForm(request.POST or None)
-
     if request.method == "POST":
+        form = UserCreationForm(request.POST)
+
         if form.is_valid():
-            try:
-                user = form.save()
-            except IntegrityError:
-                form.add_error("username", "Tên tài khoản đã tồn tại.")
-                messages.error(request, "❌ Tên tài khoản đã tồn tại.")
-            else:
-                login(request, user)
-                messages.success(request, "✅ Đăng ký thành công!")
-                return redirect("home")
-        else:
-            messages.error(request, "❌ Dữ liệu chưa hợp lệ. Kiểm tra lại form.")
+            user = form.save()
+            login(request, user)
+            messages.success(request, "Đăng ký tài khoản thành công!")
+            return redirect("home")
+    else:
+        form = UserCreationForm()
 
     return render(request, "accounts/register.html", {"form": form})
